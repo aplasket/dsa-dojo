31e86fe6560e67fdeae89c788c7dd8329343cef6
Ashley Plasket <ashley.plasket@gmail.com>
2023-11-09 15:39:21 -0500

    add additional tests for enqueue and dequeue

--- queues/test/queue_test.rb
+++ queues/test/queue_test.rb
@@ -9,6 +9,9 @@ class StackTest < Minitest::Test
     queue.enqueue("B")
     queue.enqueue("C")
     assert_equal(3, queue.count)
+    assert_equal("A", queue.head.data)
+    assert_equal("B", queue.head.next_node.data)
+    assert_equal("C", queue.head.next_node.next_node.data)
   end
 
   def test_it_dequeues
@@ -17,8 +20,11 @@ class StackTest < Minitest::Test
     queue.enqueue("B")
     queue.enqueue("C")
     assert_equal("A", queue.dequeue)
+    assert_equal("B", queue.head.data)
     assert_equal("B", queue.dequeue)
+    assert_equal("C", queue.head.data)
     assert_equal("C", queue.dequeue)
+    assert_nil(nil, queue.head)
   end
 
 end
