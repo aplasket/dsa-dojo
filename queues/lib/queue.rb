b125739a50764b7bf283002925cc0600e33cb674
Ashley Plasket <ashley.plasket@gmail.com>
2023-11-09 15:40:02 -0500

    add initialize method and enqueue method

--- queues/lib/queue.rb
+++ queues/lib/queue.rb
@@ -1,3 +1,22 @@
 class Queue
+  attr_reader :head
 
+  def initialize
+    @head = nil
+  end
+
+  def enqueue(element)
+    new_node = Node.new(element)
+    if @head.nil?
+      @head = new_node
+    else
+      current = @head
+      until current.next_node.nil?
+        current = current.next_node
+      end
+      current.next_node = new_node
+    end
+  end
+
+ 
 end
